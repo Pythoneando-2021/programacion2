7cc68b0311c622b78e5dd6510df3a2f0615e0a9d
NicoDiaz97 <nicoagudiaz97@gmail.com>
2021-05-12 22:03:17 -0300

    Ejercicio 5 NO TERMINADO (Resuelve NiKO)

--- 07-05/ejercicio_5.py
+++ 07-05/ejercicio_5.py
@@ -3,3 +3,30 @@
 precios en otra. Definir dos listas paralelas. Mostrar cuantos productos tienen un precio
 mayor al primer producto ingresado.
 """
+cantidad = int(input("Ingrese cantidad de productos: "))
+productos = []
+precios = []
+productosaux = []
+preciosaux =[]
+contador = 0
+
+for x in range(cantidad):
+    producto = input("Ingrese producto: ")
+    productos.append(producto)
+    precio = int(input("Ingrese el precio del producto:"))
+    precios.append(precio)
+
+for k in precios:
+    if k > precios[0]:
+        productosaux.append(productos[contador])
+        preciosaux.append(precio[contador])
+        contador +=1
+    else:
+        contador += 1
+        continue
+
+print("Los productos con mayor precio al primero ingresado son: ")
+
+for x in range(cantidad):
+    print(productosaux[x],preciosaux[x])
+
